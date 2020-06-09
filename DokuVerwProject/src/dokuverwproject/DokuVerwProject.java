3fcdb18d1072e7acfed12705ca0836f3d812487e
Giuseppe <Giuseppe@192.168.0.8>
2020-06-09 07:15:40 +0200

    Login-Fenster (Mockup) angelegt - Giuseppe

--- DokuVerwProject/src/dokuverwproject/DokuVerwProject.java
+++ DokuVerwProject/src/dokuverwproject/DokuVerwProject.java
@@ -5,17 +5,40 @@
  */
 package dokuverwproject;
 
+import dokuverwproject.GUI.LoginFrame;
+
 /**
  *
  * @author Giuseppe
  */
 public class DokuVerwProject {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String args[]) {
+
+        // set design to Windows-Style
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(LoginFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(LoginFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(LoginFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(LoginFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        /* Create and display LoginForm */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new LoginFrame().setVisible(true);
+            }
+        });
     }
     
 }
